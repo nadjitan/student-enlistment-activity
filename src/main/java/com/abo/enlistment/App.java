0d8189da2d61e29270d9ba6e32acc8f09eb5bfe9
J-Habal <jeremy.habal@ciit.edu.ph>
2021-04-02 19:36:01 +0800

    Added period constraints

--- src/main/java/com/abo/enlistment/App.java
+++ src/main/java/com/abo/enlistment/App.java
@@ -1,20 +1,13 @@
 package com.abo.enlistment;
 
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-
 /**
  * Hello world!
  *
  */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("App running...");
-
-        String str = "1800";
-        LocalTime parsedTime = LocalTime.parse(str, DateTimeFormatter.ofPattern("HHmm"));
-        System.out.println(parsedTime);
-        System.out.println((parsedTime.getHour() >= 8 && parsedTime.getHour() <= 17) ? true : false);
-        System.out.println((parsedTime.getMinute() == 30 || parsedTime.getMinute() == 00) ? true : false);
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "App running..." );
     }
 }
