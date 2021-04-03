80064c5964b03fe3d97a9e6ec6d8d09441445a56
Nadji <nadjitan@yahoo.com.ph>
2021-04-03 17:24:02 +0800

    Undo changes

--- src/main/java/com/abo/enlistment/App.java
+++ src/main/java/com/abo/enlistment/App.java
@@ -10,11 +10,5 @@ import java.time.format.DateTimeFormatter;
 public class App {
     public static void main(String[] args) {
         System.out.println("App running...");
-
-        String str = "1800";
-        LocalTime parsedTime = LocalTime.parse(str, DateTimeFormatter.ofPattern("HHmm"));
-        System.out.println(parsedTime);
-        System.out.println((parsedTime.getHour() >= 8 && parsedTime.getHour() <= 17) ? true : false);
-        System.out.println((parsedTime.getMinute() == 30 || parsedTime.getMinute() == 00) ? true : false);
     }
 }
